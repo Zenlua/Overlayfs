e1a902fa1f2e0f72186425211966807182131c38
Kakathic <kakathic@gmail.com>
2023-11-01 21:46:37 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,1 @@
-(
-while true; do
-if [ "$(gh issue view 14 | grep -cm1 CLOSED)" == 1 ];then
-gh run cancel $GITHUB_RUN_ID
-else
-sleep 1
-echo 1
-fi
-done
-) & (
-sleep 1000
-)
+gh issue comment 13 --body "Hi from GitHub CLI"
