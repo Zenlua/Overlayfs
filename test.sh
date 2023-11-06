375de7b19d99e13a854b883eedde400fd9b88b7c
Kakathic <kakathic@gmail.com>
2023-11-06 21:21:37 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ Taive () { curl -s -L -N -H "$User" --connect-timeout 20 "$1" -o "$2"; }
 
 ukl="https://dumps.tadiphone.dev/dumps/xiaomi/fuxi/-/raw/missi_phone_global-user-14-UKQ1.230705.002-V14.0.5.0.UMCMIXM-release-keys"
 mkdir -p Up
-
+ 
 Taive "$ukl/product/app/MIUIFrequentPhrase/MIUIFrequentPhrase.apk?inline=false" MIUIFrequentPhrase.apk
 java -jar apktool_2.9.0.jar d -s -f MIUIFrequentPhrase.apk -o App
 cp -rf App/res/string-vi Up
