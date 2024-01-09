9f9b6be6835497716f7c464f863e063ab63e18a1
Kẹo Kéo <kakathic@gmail.com>
2024-01-09 15:04:45 +0700

    Update post-fs-data.sh

--- module/common/post-fs-data.sh
+++ module/common/post-fs-data.sh
@@ -1,6 +1,6 @@
 # kakathic
 MODP="${0%/*}"
-#magisk && PMSK="$(magisk --path)/.magisk/mirror"
+magisk && PMSK="$(magisk --path)/.magisk/mirror"
 magisk && su="su -M -c"
 
 # start overlay_fs
@@ -9,6 +9,6 @@ for TV in $(cat $MODP/partition); do
 [ -e $MODP/zption/chmod ] && chmod -R 755 $MODP$TV
 [ -e $MODP/zption/chown ] && chown -Rh 0:0 $MODP$TV 
 [ -e $MODP/zption/rw ] && $su mount -t overlay kakathic -o ro,upperdir=$MODP$TV,lowerdir=$TV,workdir=$MODP/zption/tmp1$TV $TV || $su mount -t overlay kakathic -o lowerdir=$MODP$TV:$TV $TV
-#[ -e $MODP/zption/rw ] && $su mount -t overlay kakathic -o upperdir=$MODP$TV,lowerdir=$PMSK$TV,workdir=$MODP/zption/tmp2$TV $PMSK$TV || $su mount -t overlay kakathic -o lowerdir=$MODP$TV:$PMSK$TV $PMSK$TV
+[ -e $MODP/zption/rw ] && $su mount -t overlay kakathic -o upperdir=$MODP$TV,lowerdir=$PMSK$TV,workdir=$MODP/zption/tmp2$TV $PMSK$TV || $su mount -t overlay kakathic -o lowerdir=$MODP$TV:$PMSK$TV $PMSK$TV
 done
 # end overlay_fs
