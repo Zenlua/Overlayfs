4b38e9c0dc1c5b72369db30a1d42e9de8fca9b1e
Kẹo Kéo <kakathic@gmail.com>
2024-01-09 07:25:21 +0700

    Update Shell.sh

--- .github/Shell.sh
+++ .github/Shell.sh
@@ -3,7 +3,19 @@ TOME="$GITHUB_WORKSPACE"
 mkdir -p $TOME/Up
 
 getmodun(){ grep -m1 "$1=" $TOME/module/module.prop | cut -d= -f2; }
+upenv(){ echo "$1=$2" >> $GITHUB_ENV; }
 
 cd $TOME/module
 
 zip -r $TOME/Up/$(getmodun id)_$(getmodun version).zip *
+
+upenv "V_$(getmodun version)"
+
+echo '
+{
+"version": "'$(getmodun version)'",
+"versionCode": "'$(getmodun versionCode)'",
+"zipUrl": "kkkkkk",
+"changelog": "kkkk"
+}
+' > $TOME/tmp2/overlay.json
