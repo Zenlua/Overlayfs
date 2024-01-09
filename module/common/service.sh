bff15b9505797465775951b781652928bc90f8fd
Kẹo Kéo <kakathic@gmail.com>
2024-01-09 15:23:13 +0700

    Update service.sh

--- module/common/service.sh
+++ module/common/service.sh
@@ -9,7 +9,14 @@ done
 # end
 
 # mount rw overlayfs
+if [ -e $MODP/zption/rw ];then
+while true; do
+# mount rw overlayfs
 for TV in $(cat $MODP/partition); do
-[ -e $MODP/zption/rw ] && mount -o rw,remount $TV
-[ -e $MODP/zption/rw ] && mount -o rw,remount $PMSK$TV
+mount -o rw,remount $TV
+mount -o rw,remount $PMSK$TV
 done
+[ "$(mount -t overlay | grep -m1 kakathic | grep -cm1 'rw')" == 1 ] && break
+[ $kakatest -gt 100 ] && break || kakatest=$(($kakatest + 1))
+done
+fi
