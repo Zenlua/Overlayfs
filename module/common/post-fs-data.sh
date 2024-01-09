87f95e9ca60f6d9c46abd0d8b6fd1cdde19969a9
Kẹo Kéo <kakathic@gmail.com>
2024-01-09 18:33:37 +0700

    Update post-fs-data.sh

--- module/common/post-fs-data.sh
+++ module/common/post-fs-data.sh
@@ -1,6 +1,6 @@
 # kakathic
 MODP="${0%/*}"
-magisk && PMSK="$(magisk --path)/.magisk/mirror"
+#magisk && PMSK="$(magisk --path)/.magisk/mirror"
 
 # start overlay_fs
 for TV in $(cat $MODP/partition); do
@@ -9,10 +9,10 @@ for TV in $(cat $MODP/partition); do
 [ -e $MODP/zption/chown ] && chown -Rh 0:0 $MODP$TV
 if [ -e $MODP/zption/rw ];then
 mount -t overlay kakathic -o upperdir=$MODP$TV,lowerdir=$TV,workdir=$MODP/zption/tmp1$TV $TV
-magisk && mount -t overlay kakathic -o upperdir=$MODP$TV,lowerdir=$PMSK$TV,workdir=$MODP/zption/tmp2$TV $PMSK$TV
+#magisk && mount -t overlay kakathic -o upperdir=$MODP$TV,lowerdir=$PMSK$TV,workdir=$MODP/zption/tmp2$TV $PMSK$TV
 else
 mount -t overlay kakathic -o lowerdir=$MODP$TV:$TV $TV
-magisk && mount -t overlay kakathic -o lowerdir=$MODP$TV:$PMSK$TV $PMSK$TV
+#magisk && mount -t overlay kakathic -o lowerdir=$MODP$TV:$PMSK$TV $PMSK$TV
 fi
 done
 # end overlay_fs
