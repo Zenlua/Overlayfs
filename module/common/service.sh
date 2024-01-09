eb55d472a947b63109ff24e8dff13abd6bcfbbfb
Kẹo Kéo <kakathic@gmail.com>
2024-01-09 15:05:46 +0700

    Update service.sh

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,5 +1,6 @@
 # kakathic
 MODP="${0%/*}"
+magisk && PMSK="$(magisk --path)/.magisk/mirror"
 
 # Check boot
 while true; do
@@ -12,6 +13,7 @@ while true; do
 # mount rw overlayfs
 for TV in $(cat $MODP/partition); do
 mount -o rw,remount $TV
+mount -o rw,remount $PMSK$TV
 done
 [ "$(mount -t overlay | grep -m1 kakathic | grep -cm1 'rw')" == 1 ] && break
 [ $kakatest -gt 100 ] && break || kakatest=$(($kakatest + 1))
