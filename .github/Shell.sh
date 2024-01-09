ce9ba2b1f7df96da63493c313f575f2ba9b5565e
Kẹo Kéo <kakathic@gmail.com>
2024-01-09 07:13:16 +0700

    Update Shell.sh

--- .github/Shell.sh
+++ .github/Shell.sh
@@ -1,1 +1,9 @@
-test
+TOME="$GITHUB_WORKSPACE"
+
+mkdir -p $TOME/Up
+
+getmodun(){ grep -m1 "$1=" $TOME/module/module.prop | cut -d= -f2; }
+
+cd $TOME/module
+
+zip -r $TOME/Up/$(getmodun id)_$(getmodun version).zip *
