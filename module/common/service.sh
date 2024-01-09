269f0a8f5c8115fd1fb67e0d878ead8dc9f44a01
Kẹo Kéo <kakathic@gmail.com>
2024-01-09 14:48:15 +0700

    Update service.sh

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,9 +1,19 @@
 # kakathic
 MODP="${0%/*}"
-PMSK="$(magisk --path)/.magisk/mirror"
 
 # Check boot
 while true; do
-[ -e /sdcard/Android ] && break || sleep 1
+[ "$(getprop sys.boot_completed)" == 1 ] && break || sleep 1
 done
 # end
+
+if [ -e $MODP/zption/rw ];then
+while true; do
+# mount rw overlayfs
+for TV in $(cat $MODP/partition); do
+mount -o rw,remount $TV
+done
+[ "$(mount -t overlay | grep -m1 kakathic | grep -cm1 'rw')" == 1 ] && break
+[ $kakatest -gt 100 ] && break || kakatest=$(($kakatest + 1))
+done
+fi
