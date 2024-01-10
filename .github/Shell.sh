82a0b5882e10ea3be2348b104191ef12f4f52f52
Kẹo Kéo <kakathic@gmail.com>
2024-01-10 21:10:59 +0700

    Update Shell.sh

--- .github/Shell.sh
+++ .github/Shell.sh
@@ -3,13 +3,14 @@ TOME="$GITHUB_WORKSPACE"
 mkdir -p $TOME/Up $TOME/tmp2
 
 getmodun(){ grep -m1 "$1=" $TOME/module/module.prop | cut -d= -f2; }
-upenv(){ echo "$1=$2" >> $GITHUB_ENV; eval "$1=$2"; }
+upenv(){ echo "$1='$2'" >> $GITHUB_ENV; eval "$1='$2'"; }
 
 cd $TOME/module
 
 zip -r $TOME/Up/$(getmodun id)_$(getmodun version).zip *
 upenv VER "V$(getmodun version)"
 upenv VUR "$(getmodun version)"
+upenv BOR "$(cat $TOME/module/log.md | sed -n "/#### $VER/,/####/P" | sed "/#/d")"
 
 echo '
 {
