3918b78bda59ea5cf14f9901aa937ce0ebd1e852
Kẹo Kéo <kakathic@gmail.com>
2024-01-17 10:04:03 +0700

    Update install.sh

--- module/install.sh
+++ module/install.sh
@@ -18,23 +18,10 @@ ui_print "  Name: $(grep_prop name $TMPDIR/module.prop), $(grep_prop version $TM
 # Bắt đầu cài đặt
 on_install() {
 ui_print
-ui_print "  Create folder"
+ui_print "  Create overlayrw"
 ui_print
-for KS1 in $(grep_prop partition $TMPDIR/module.prop); do
-ui_print "  $KS1"
-echo "$KS1" >> $MODPATH/partition
-mkdir -p $MODPATH$KS1
-setfattr -n trusted.overlay.opaque -v y $MODPATH$KS1
-mkdir -p $MODPATH/zption/tmp$KS1
-#mkdir -p $MODPATH/zption/tmp2$KS1
-done
-# tạo tùy chọn 
-[ "$(grep_prop chmod $TMPDIR/module.prop)" == "true" ] && echo > $MODPATH/zption/chmod
-[ "$(grep_prop chown $TMPDIR/module.prop)" == "true" ] && echo > $MODPATH/zption/chown
-[ "$(grep_prop chcon $TMPDIR/module.prop)" == "true" ] && echo > $MODPATH/zption/chcon
-# backup
-if [ "$(grep_prop backup $TMPDIR/module.prop)" == "true" ] && [ -e "/data/adb/modules/overlayfs/skip_mount" ];then
-ui_print
+mkdir -p $MODPATH/system/bin
+cp -rf $TMPDIR/overlayrw $MODPATH/system/bin
 ui_print "  Start backup"
 for KS3 in $(ls -d1 /data/adb/modules/overlayfs/*); do
 [ -d "$KS3" ] && cp -acf $KS3 $MODPATH
@@ -45,6 +32,5 @@ ui_print
 
 # Cấp quyền
 set_permissions() { 
-#set_perm_recursive $MODPATH 0 0 0755 0644
-true
+set_perm_recursive $MODPATH/system/bin 0 0 0755 0755
 }
