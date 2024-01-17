8d82c9c8b2183b1433ca89576b61b46ec2483b58
Kẹo Kéo <kakathic@gmail.com>
2024-01-18 02:38:06 +0700

    Update service.sh

--- module/common/service.sh
+++ module/common/service.sh
@@ -6,6 +6,6 @@ while true; do
 [ "$(getprop sys.boot_completed)" == 1 ] && break || sleep 1
 done
 
-for TV in $(getprop 'partition=' $MODP/module.prop | cut -d= -f2); do
+for TV in $(grep 'partition=' $MODP/module.prop | cut -d= -f2); do
 magisk || overlayrw $TV
 done
