11617229e10ef54889047cc14b2fb6adc0d641c8
Kẹo Kéo <kakathic@gmail.com>
2024-01-18 02:26:25 +0700

    Update post-fs-data.sh

--- module/common/post-fs-data.sh
+++ module/common/post-fs-data.sh
@@ -1,9 +1,6 @@
 # kakathic
 MODP="${0%/*}"
 
-magisk && cp -rf $MODP/system/bin/* $MODP
-magisk && rm -fr $MODP/system
-
 for TV in $(getprop 'partition=' $MODP/module.prop | cut -d= -f2); do
 magisk && $MODP/overlayrw $TV
 done
