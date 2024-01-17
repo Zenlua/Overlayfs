a7e7d8e4064f77765fffbf56a605c6c974cc5921
Kẹo Kéo <kakathic@gmail.com>
2024-01-17 09:41:46 +0700

    Update post-fs-data.sh

--- module/common/post-fs-data.sh
+++ module/common/post-fs-data.sh
@@ -1,3 +1,9 @@
 # kakathic
 MODP="${0%/*}"
-rm -fr $MODP/zption/check
+for TV in $(cat $MODP/partition); do
+if [ ! -L $TV ];then
+[ -e $MODP/zption/chcon ] && chcon -Rh "$(ls -Z -d $TV | awk '{print $1}')" $MODP$TV
+[ -e $MODP/zption/chmod ] && chmod -R 755 $MODP$TV
+[ -e $MODP/zption/chown ] && chown -Rh 0:0 $MODP$TV
+fi
+done
