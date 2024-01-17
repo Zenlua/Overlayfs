fa463d4e339ba162edda8357281339f47ffc6fc9
Kẹo Kéo <kakathic@gmail.com>
2024-01-18 02:14:28 +0700

    Update service.sh

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,9 +1,11 @@
 # kakathic
 MODP="${0%/*}"
 #MSGP="$(magisk --path 2>/dev/null)/.magisk/mirror"
+
 while true; do
 [ "$(getprop sys.boot_completed)" == 1 ] && break || sleep 1
 done
-for vv in $list; do
-overlayrw $vv
+
+for TV in $(getprop 'partition=' $MODP/module.prop | cut -d= -f2); do
+magisk || overlayrw $TV
 done
