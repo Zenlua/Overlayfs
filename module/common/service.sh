d2d261c5355290a918b1a5e2165295df2e687937
Kẹo Kéo <kakathic@gmail.com>
2024-01-18 02:57:44 +0700

    Update service.sh

--- module/common/service.sh
+++ module/common/service.sh
@@ -3,7 +3,11 @@ MODP="${0%/*}"
 #MSGP="$(magisk --path 2>/dev/null)/.magisk/mirror"
 
 while true; do
-[ "$(getprop sys.boot_completed)" == 1 ] && break || sleep 1
+bodem=$(($bodem + 1))
+[ "$(getprop sys.boot_completed)" == 1 ] && break
+[ "$bodem" -ge 180 ] && echo > $MODP/disable
+[ "$bodem" -ge 180 ] && reboot
+sleep 1
 done
 
 for TV in $(grep 'partition=' $MODP/module.prop | cut -d= -f2); do
