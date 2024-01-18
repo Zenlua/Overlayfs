748b739a971a8adf71e750ea3a35e5463b179b5b
Kẹo Kéo <kakathic@gmail.com>
2024-01-18 12:01:58 +0700

    Update post-fs-data.sh

--- module/common/post-fs-data.sh
+++ module/common/post-fs-data.sh
@@ -1,6 +1,8 @@
 # kakathic
 MODP="${0%/*}"
+rm -fr $MODP/log.txt
+[ "$(grep 'vipmount=' $MODP/module.prop | cut -d= -f2)" == 2 ] && echo > $MODP/skip_mount || rm -fr $MODP/skip_mount
 
 for TV in $(grep 'partition=' $MODP/module.prop | cut -d= -f2); do
-[ "$(magisk --path)" ] && $MODP/product/bin/overlayrw $TV
+[ "$(grep 'vipmount=' $MODP/module.prop | cut -d= -f2)" == 2 ] && $MODP/system/product/bin/overlayrw $TV >> $MODP/log.txt
 done
