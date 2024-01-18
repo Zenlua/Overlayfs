7ca51efb95e068fd4f88c3034e8690484880abdd
Kẹo Kéo <kakathic@gmail.com>
2024-01-18 11:57:53 +0700

    Update Shell.sh

--- .github/Shell.sh
+++ .github/Shell.sh
@@ -10,13 +10,15 @@ cd $TOME/module
 zip -r $TOME/Up/$(getmodun id)_$(getmodun version).zip *
 upenv VER "V$(getmodun version)"
 upenv VUR "$(getmodun version)"
+upenv VSR "$(getmodun versionCode)"
+upenv VIR "$(getmodun id)"
 cat $TOME/module/log.md | sed -n "/#### $VER/,/####/P" | sed "/#/d" | sed '1d' | sed '$d' >$TOME/log.txt
 
 echo '
 {
-"version": "'$(getmodun version)'",
-"versionCode": "'$(getmodun versionCode)'",
-"zipUrl": "https://github.com/Zenlua/Overlayfs/releases/download/'$VER'/'$(getmodun id)_$(getmodun version)'.zip",
+"version": "'$VUR'",
+"versionCode": "'$VSR'",
+"zipUrl": "https://github.com/Zenlua/Overlayfs/releases/download/'$VER'/'$VIR_$VUR'.zip",
 "changelog": "https://raw.githubusercontent.com/Zenlua/Overlayfs/main/module/log.md"
 }
 ' > $TOME/tmp2/overlay.json
