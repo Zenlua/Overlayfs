372e3a57587b850a246d9cb5eafd15260ce67cfd
Kẹo Kéo <kakathic@gmail.com>
2024-01-20 12:07:34 +0700

    Update post-fs-data.sh

--- module/common/post-fs-data.sh
+++ module/common/post-fs-data.sh
@@ -2,5 +2,5 @@
 MODP="${0%/*}"
 rm -fr $MODP/log.txt
 for TV in $(cat /data/overlayfs/tmp/partition); do
-[ "$(grep 'vipmount=' $MODP/module.prop | cut -d= -f2)" == 1 ] || /data/overlayfs/system/product/bin/overlayrw $TV >> $MODP/log.txt
+[ "$(grep 'vipmount=' $MODP/module.prop | cut -d= -f2)" == 1 ] && mount -t overlay kakathic -o "lowerdir=$MODP$TV:$TV" "$TV" || /data/overlayfs/system/product/bin/overlayrw $TV >> $MODP/log.txt
 done
