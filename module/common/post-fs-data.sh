4bd23ab6d8e0a6acc7d82a4c6486b4ef04965286
Kẹo Kéo <kakathic@gmail.com>
2024-01-20 11:44:32 +0700

    Update post-fs-data.sh

--- module/common/post-fs-data.sh
+++ module/common/post-fs-data.sh
@@ -1,6 +1,6 @@
 # kakathic
 MODP="${0%/*}"
 rm -fr $MODP/log.txt
-for TV in $(grep 'partition=' $MODP/module.prop | cut -d= -f2); do
+for TV in $(cat $MODP/partition); do
 /data/overlayfs/system/product/bin/overlayrw $TV >> $MODP/log.txt
 done
