fa93d80e9b4d4b6162bae820bd2aed2fe95824a7
Kẹo Kéo <kakathic@gmail.com>
2024-01-21 15:46:37 +0700

    Update service.sh

--- module/common/service.sh
+++ module/common/service.sh
@@ -13,3 +13,5 @@ done
 for TV in $(cat /data/overlayfs/tmp/partition); do
 [ "$(grep 'vipmount=' "$MODP/module.prop" | cut -d= -f2)" == 1 ] && overlayrw "$TV" >> "$MODP/log.txt" 2>> "$MODP/log.txt"
 done
+
+mount -t overlay > $MODP/overlay.txt
