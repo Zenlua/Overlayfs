5690a1fe69d33036806780d8c09eefdb31951134
Kẹo Kéo <kakathic@gmail.com>
2024-01-25 14:05:14 +0700

    Update install.sh

--- module/install.sh
+++ module/install.sh
@@ -30,7 +30,7 @@ done
 Text="$(cat /data/overlayfs/tmp/partition | sort | uniq)"
 echo "$Text" > /data/overlayfs/tmp/partition
 cp -rf $TMPDIR/overlayrw /data/overlayfs/system/product/bin
-[ -e "$(magisk --path)/.magisk/mirror/system” ] && sed -i "s|vipmount=2|vipmount=1|g" $TMPDIR/module.prop
+[ -e "$(magisk --path)/.magisk/mirror/system" ] && sed -i "s|vipmount=2|vipmount=1|g" $TMPDIR/module.prop
 ui_print " "
 ui_print "  Save: /data/overlayfs"
 ui_print " "
