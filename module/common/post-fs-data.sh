17f6aff999ec61d1c8e06392d88cc6075f2ca8b9
Kẹo Kéo <kakathic@gmail.com>
2024-01-25 14:14:39 +0700

    Update post-fs-data.sh

--- module/common/post-fs-data.sh
+++ module/common/post-fs-data.sh
@@ -2,10 +2,10 @@
 MODP="${0%/*}"
 rm -fr $MODP/log.txt
 if [ "$(grep 'vipmount=' "$MODP/module.prop" | cut -d= -f2)" == 1 ];then
-rm -fr "$MODP/disable"
+rm -fr "$MODP/skip_mount"
 mount -o bind /data/overlayfs/system "$MODP/system"
 else
-echo > "$MODP/disable"
+echo > "$MODP/skip_mount"
 for TV in $(cat /data/overlayfs/tmp/partition); do
 /data/overlayfs/system/product/bin/overlayrw "$TV" >> "$MODP/log.txt" 2>> "$MODP/log.txt"
 done
