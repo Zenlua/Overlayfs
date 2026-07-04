54c51d85bc3187a07127516e3c692ac7545b29e6
Kakathic <kakathic@gmail.com>
2026-07-04 16:50:19 +0700

    Add files via upload

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,17 +1,41 @@
-# kakathic
-MODP="${0%/*}"
+# Kakathic
+
 #MSGP="$(magisk --path 2>/dev/null)/.magisk/mirror"
+#MDS="/data/adb/modules_update/overlayfs"
+MKD="${0%/*}"
+
+overlayrw(){
+if [ -e "$2" ];then
+    mkdir -p "$MKD$2"
+    [ "$1" == "rw" ] && mkdir -p "$MKD/tmp$2"
+    busybox setfattr -n trusted.overlay.opaque -v y $MKD$2 2>/dev/null
+    chcon "$(busybox ls -nZld "$2" | awk '{print $5}')" "$MKD$2"
+    chmod 755 "$MKD$2"
+    chown $(busybox ls -nZld "$2" | awk '{print $3":"$4}') "$MKD$2"
+fi
+if [ "$1" == "ro" ];then
+    mount -t overlay Kakathic -o "lowerdir=$MODP$2:$2" "$2" && echo "Mount RO: $2 done"
+elif [ "$1" == "rw" ];then
+    mount -t overlay Kakathic -o "upperdir=$MKD$2,lowerdir=$2,workdir=$MKD/tmp$2" "$2" && echo "Mount RW: $2 done"
+fi
+}
+
+# Run code
+[ -f $MKD/log.txt ] && rm -fr $MKD/log.txt
+for vcl in $(cat $MKD/partition.txt | sort | uniq); do
+[ -d "$vcl" ] && overlayrw rw "$vcl" >> "$MKD/log.txt" 2>> "$MKD/log.txt"
+done
 
 # Anti-bootloop 120s
 while true; do
 bodem=$(($bodem + 1))
 [ "$(getprop sys.boot_completed)" == 1 ] && break
-[ "$bodem" -ge 120 ] && echo > $MODP/disable
-[ "$bodem" -ge 120 ] && reboot
+if [ "$bodem" -ge 120 ]; then
+echo > $MKD/disable
+reboot
+fi
 sleep 1
 done
 
 # log overlay
-[ -e "$MODP/bind" ] && mount | grep 'rw.*.noauto_da_alloc.*.data=ordered' > $MODP/overlay.txt || mount -t overlay > $MODP/overlay.txt
-
-echo "/data/overlayfs/$(getprop ro.build.version.incremental)" > /data/overlayfs/tmp/pathxt
+mount -t overlay > $MKD/overlay.txt
