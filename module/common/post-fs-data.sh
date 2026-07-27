a8de982f2557237e7ffbfc50cad24cd36a6b7ad8
Kakathic <kakathic@gmail.com>
2026-07-27 20:21:59 +0700

    Update post-fs-data.sh

--- module/common/post-fs-data.sh
+++ module/common/post-fs-data.sh
@@ -9,7 +9,13 @@ mount --bind "$MKD" "$MKS"
 
 # tính năng
 overlayfs_ro(){
-    [ -d "$MKS$1" ] && mount -t overlay overlay -o "lowerdir=$MKS$1:$1" "$1" && echo "Mount RO: $1 done"
+    if [ -d "$MKS$1" ]; then
+    busybox setfattr -n trusted.overlay.opaque -v y $MKD$1 2>/dev/null
+    chcon -R "$(busybox ls -nZld "$1" | awk '{print $5}')" "$MKD$1"
+    chmod -R 755 "$MKD$1"
+    chown -R $(busybox ls -nZld "$1" | awk '{print $3":"$4}') "$MKD$1"
+    mount -t overlay overlay -o "lowerdir=$MKS$1:$1" "$1" && echo "Mount RO: $1 done"
+    fi
 }
 
 # run overlay
